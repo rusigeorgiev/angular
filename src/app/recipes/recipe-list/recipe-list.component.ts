f3c75e74260b8427fc38db9a6faae50b6fe2074e
BUDAPEST\Rusi_Georgiev <Rusi_Georgiev@epam.com>
2017-09-13 15:47:21 +0300

    Add shopping list details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Soft center cake', 'The best soft center cake',
+    'http://www.sanjeevkapoor.com/UploadFiles/RecipeImages/Soft_Centered_Chocolate_Cakes_khanakhazana.jpg'),
+    new Recipe('Пиле със зеле', 'Много яка рецепта за пиле със зеле',
+    'http://www.supichka.com/files/images/268/resize_535_2000.jpg')
+  ];
 
   constructor() { }
 
